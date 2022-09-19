baebe1ed7ed570dbbb764e14f93916948b729af3
reza <reza@localhost>
2022-09-19 09:47:29 -0700

    Test

--- testProject/src/test305/test.java
+++ testProject/src/test305/test.java
@@ -4,7 +4,7 @@ import java.lang.Math;
 public class test {
 	public static void main(String args[])
 	{
-		System.out.println("1st Random Number: " + Math.random());
+		System.out.println("Random Number: " + Math.random());
 	}
 
 }
