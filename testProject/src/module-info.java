ca50eb125b72fda961c74f693eb8bdc53c404d35
reza <reza@localhost>
2022-09-19 09:35:57 -0700

    Test

---
+++ testProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author reza
+ *
+ */
+module testProject {
+}
---
+++ testProject/src/test305/test.java
@@ -0,0 +1,10 @@
+package test305;
+import java.lang.Math;
+
+public class test {
+	public static void main(String args[])
+	{
+		System.out.println("Random Number: " + Math.random());
+	}
+
+}
